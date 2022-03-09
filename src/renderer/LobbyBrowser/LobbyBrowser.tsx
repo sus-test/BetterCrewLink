2ac1077b89787cfad62bd3cc45bf3f557c55f269
sus-test <79755732+sus-test@users.noreply.github.com>
2022-03-09 20:27:49 -0300

    Change the IP to a CNAME (domain)
    Try to see if works

--- src/renderer/LobbyBrowser/LobbyBrowser.tsx
+++ src/renderer/LobbyBrowser/LobbyBrowser.tsx
@@ -63,7 +63,7 @@ const servers: {
 	'139.162.111.196': 'Asia',
 	'192.241.154.115': 'skeld.net',
 	'185.7.80.9': 'TOU Master',
-	'78.47.142.18': 'TOR Server',
+	'play.theotherroles.de': 'TOR Server',
 };
 
 function sortLobbies(a: PublicLobby, b: PublicLobby) {
